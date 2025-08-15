33b648c642c4f2b30a3e86dc6ba4eb03f9d703d1
Tam T. Pham <tamphamweb@gmail.com>
2025-08-14 23:34:55 -0700

    Added requirement for flask cors to pip and additional call inside API. The headers are required in modern browsers.

--- restAPI.py
+++ restAPI.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 import PrimeCalculator
 from PrimeCalculator import miller_rabin, sieveOfAtkin
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def root():
